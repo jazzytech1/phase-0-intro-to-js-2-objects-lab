c6b8b768ec390acdef34693a29a75fab65cd8ddb
Kawana T Small <jazzytech@Kawanas-MacBook-Pro.local>
2022-03-29 11:35:47 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution in this file!
+let employee = {
+name: "Jazzy",
+streetAddress:"Harlem"
+}
+
+  function updateEmployeeWithKeyAndValue (employee,key,value){
+      let newObject = {
+          ...employee
+        
+      };
+      newObject[key]= value;
+      return newObject;
+  }
+
+  function destructivelyUpdateEmployeeWithKeyAndValue (employee, key, value){
+  let newObject = {
+    ...employee
+  
+};
+employee[key]= value;
+      return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    let newObject = {
+        ...employee
+      
+    };
+    delete newObject[key];
+    return newObject;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    let newObject = {
+        ...employee
+      
+    };
+    delete employee[key];
+    return employee;
+}
+  
