bb6f10614dbcb600346219ee2336387adf9e972c
Kawana T Small <jazzytech@Kawanas-MacBook-Pro.local>
2022-07-06 20:14:20 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your solution in this file!
-let employee = {
+/*let employee = {
 name: "Jazzy",
 streetAddress:"Harlem"
 }
@@ -38,4 +38,51 @@ function destructivelyDeleteFromEmployeeByKey(employee, key){
     delete employee[key];
     return employee;
 }
-  
+  */
+
+
+
+
+const employee = {
+name:"Kennedy Small" }
+streetAddress: {
+    street:"129 West 147th Street"
+    apt: "#24F"
+    city:"New York"
+    state:"NY"
+    zipCode:"10030" }
+function updateEmployeeWithKeyAndValue (employee,key,value){
+    const newEmployee = {...employee};
+    newEmployee [key]=value;
+    return newEmployee
+    }
+    function destructivelyUpdateEmployeeWithKeyAndValue (employee,key,value){
+        employee [key]=value;
+        return employee   
+}
+function deleteFromEmployeeByKey (employee,key){
+    const newEmployee = {...employee};  
+    delete newEmployee[key];
+    return newEmployee
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee
+}
+
+   
+    
+
+    
+
+        
+    
+
+
+    
+    
+
+
+
+
+
